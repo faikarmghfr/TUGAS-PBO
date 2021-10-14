3e808c0b9bf5b4bda97d673c064bddb59494779d
Faikar M Ghifari <faikar.mochammad.tif20@polban.ac.id>
2021-10-15 01:02:21 +0700

    minggu5.2

---
+++ minggu4/src/minggu5.2/src/minggu5/pkg2/Sasuke.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu5.pkg2;
+
+/**
+ *
+ * @author faikar
+ */
+public class Sasuke extends itachi{
+    String Dojutsu = "Sharingan";
+
+	void printDojutsu() {
+		super.printDojutsu();
+		System.out.println(this.Dojutsu);
+	}
+}
---
+++ minggu4/src/minggu5.2/src/minggu5/pkg2/narutoAnime.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu5.pkg2;
+
+/**
+ *
+ * @author faikar
+ */
+public class narutoAnime {
+    public static void main(String[] args) {
+		Sasuke s = new Sasuke();
+		s.printDojutsu();
+		itachi i = new Sasuke();
+		i.printKekkeiGenkai();
+	}
+}
